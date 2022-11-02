d9b10d91bc86cb98cae1d1db132da011a21fecdd
Ashish <ashish.chaudhary008@gmail.com>
2022-11-02 10:01:16 +0530

    Added leet code 69. Sqrt(x)

--- src/dsa450/array/easy/SquareRootOfNumber.java
+++ src/dsa450/array/easy/SquareRootOfNumber.java
@@ -71,13 +71,51 @@ public class SquareRootOfNumber {
             else if (mid < (N / mid)) {
                 ans = mid;
                 low = mid + 1;
-            }
-            else {
+            } else {
                 high = mid - 1;
             }
         }
 
         return (int) ans;
+    }
+
+    /*
+    Leet code sqrt method problem.
+    Solution2 - Newton Solution: Time complexity = O(lg(x))
+
+    I think Newton solution will not be faster than Solution1(Binary Search), because i = (i + x / i) / 2, the two factors i and x / i are with opposite trends. So time complexity in the best case is O(lgx).
+
+    Anyone can give the accurate time complexity? Appreciate it!
+
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        long i = x;
+        while(i > x / i)
+            i = (i + x / i) / 2;
+        return (int)i;
+    }
+    Solution3 - Brute Force: Time complexity = O(sqrt(x))
+
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        for (int i = 1; i <= x / i; i++)
+            if (i <= x / i && (i + 1) > x / (i + 1))// Look for the critical point: i*i <= x && (i+1)(i+1) > x
+                return i;
+        return -1;
+    }
+     */
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int start = 1, end = x;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1))// Found the result
+                return mid;
+            else if (mid > x / mid)// Keep checking the left part
+                end = mid;
+            else start = mid + 1;// Keep checking the right part
+        }
+        return start;
     }
 
 
